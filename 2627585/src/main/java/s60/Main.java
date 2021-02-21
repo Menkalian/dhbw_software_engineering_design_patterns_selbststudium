12d49a031ee5a688216f1948c04ba289d58f51db
Kilian Krampf <kilian@krampf.de>
2021-02-21 18:02:51 +0100

    Created Main Program

---
+++ 2627585/src/main/java/s60/Main.java
@@ -0,0 +1,11 @@
+package s60;
+
+import s60.gasstation.GasStation;
+import s60.gasstation.IGasStation;
+
+public abstract class Main {
+    public static void main (String[] args) {
+        IGasStation station = new GasStation.Builder().amountCarLanes(1).amountTruckLanes(1).build();
+        station.startSimulation();
+    }
+}
